7ba26f1f9992f8b65dafe79e2119bb6f863954f9
戴森球 <3289990188@qq.com>
2024-04-08 17:10:59 +0800

    my thirded commit

---
+++ node_modules/vue3-marquee/dist/index.d.ts
@@ -0,0 +1,9 @@
+import type { Plugin } from 'vue';
+import Vue3Marquee from "./vue3-marquee";
+export * from './types';
+export interface PluginOptions {
+    name?: string;
+}
+declare const _default: Plugin;
+export default _default;
+export { Vue3Marquee };
